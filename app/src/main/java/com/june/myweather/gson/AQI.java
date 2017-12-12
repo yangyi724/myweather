8f3cc936ca495ea6b64b97d8a54a3888e8aa17e6
June <june_young@yeah.net>
2017-12-12 10:40:07 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/june/myweather/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.june.myweather.gson;
+
+/**
+ * Created by June on 2017/12/11.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
--- app/src/main/java/com/june/myweather/util/Utility.java
+++ app/src/main/java/com/june/myweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.june.myweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.june.myweather.db.City;
 import com.june.myweather.db.County;
 import com.june.myweather.db.Province;
+import com.june.myweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -84,4 +86,21 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
